a86222d1ded2200929f8bb810b811d12ef680851
znichola <znichola@student.42lausanne.ch>
2022-10-26 05:15:07 +0200

    slightly smaller function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 15:55:11 by znichola          #+#    #+#             */
-/*   Updated: 2022/10/26 04:30:18 by znichola         ###   ########.fr       */
+/*   Updated: 2022/10/26 05:14:58 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ int	initbuffer(t_rest *rest)
 	return (1);
 }
 
+char *freeret(t_rest *rest, int r, char *ret)
+{
+	free(rest->root);
+	rest->root = NULL;
+	rest->seek = NULL;
+	if (r == -1)
+		free(ret);
+	return (ret);
+}
+
 char	*seekline(int fd, t_rest *rest)
 {
 	char	*s;
@@ -36,26 +46,15 @@ char	*seekline(int fd, t_rest *rest)
 	{
 		if (rest->seek[0] == '\0')
 		{
-			// need to read from file
-			//ft_bzero(rest->root, BUFFER_SIZE);
 			read_out = read(fd, rest->root, BUFFER_SIZE);
 			rest->root[read_out] = '\0';
-			//if readout == 0 return ret rembere to free and NULL
 			if (read_out <= 0)
-			{
-				free(rest->root);
-				rest->root = NULL;
-				rest->seek = NULL;
-				return (ret);
-			}
+				return (freeret(rest, read_out, ret));
 			rest->seek = rest->root;
-			// printf("some newline{%s}\n", rest->seek);
 		}
 		s = ft_strchr(rest->seek, DELIM);
-		// printf("here {%s}\n", s);
 		if (*s != '\n')
 		{
-			// merg the full buffer
 			ret = ft_strmerg(ret, rest->seek);
 			if (!ret)
 				return (NULL);
@@ -63,7 +62,6 @@ char	*seekline(int fd, t_rest *rest)
 		}
 		else
 		{
-			// merg the remainder of what's in the buffer
 			ret = ft_strmerg(ret, rest->seek);
 			rest->seek = s + 1;
 			return (ret);
@@ -89,7 +87,6 @@ int main(int ac, char **av)
 	int fd = open("files/odyssey", O_RDONLY);
 	if (fd == -1)
 		return (0);
-	// static t_rest rest[4096];
 	
 	for (int i = 0; i < 20; i++)
 	{
